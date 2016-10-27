bfd6517e4dc47701bbf35764e89228f474bf317c
Devi Handevi <devihandroid@gmail.com>
2016-10-27 11:42:32 +0700

    [TWITTER] Followers count done, token masih blm dimasukin dulu

--- count_twitter.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$json_result = array (
-	'status' => 'ok',
-	'followers_count' => '312K'
-);
-echo json_encode($json_result);
-?>
--- index.php
+++ index.php
@@ -37,96 +37,43 @@
 </html>
 
 <?php
+function getTwitterFollowers() {
+	ini_set('display_errors', 1);
+	require_once('TwitterAPIExchange.php');
+
+	$settings = array(
+		'oauth_access_token' => "",
+		'oauth_access_token_secret' => "",
+		'consumer_key' => "",
+		'consumer_secret' => ""
+	);
+
+    $url = 'https://api.twitter.com/1.1/users/lookup.json';
+	$getfield = '?screen_name=ridwankamil';
+	$requestMethod = 'GET';
+	$twitter = new TwitterAPIExchange($settings);
+
+	$response = $twitter->setGetfield($getfield)
+				 ->buildOauth($url, $requestMethod)
+				 ->performRequest();
+				 
+	$mil = 1000000;
+	$thou = 1000;
+	
+	$json_data = json_decode($response, true);
+	$followers_count = $json_data[0]['followers_count'];
+	if($followers_count >= $mil) {
+		return number_format((float)$followers_count/$mil, 2, '.', '') ."M";
+	} else if($followers_count >= $thou) {
+		return number_format((float)$followers_count/$thou, 2, '.', '') ."K";
+	} else {
+		return $followers_count;
+	}
+}
+
+
 function getFacebookFollowers() {
     $numberOfFollowers = "124K";
     return $numberOfFollowers;
 }
-
-function getTwitterFollowers() {
-    $numberOfFollowers = "312K";
-    return $numberOfFollowers;
-}
-
-	//function getTwitterFollowers($screenName = 'wpbeginner') {
-	/*
-	$follow_count = file_get_contents('count_twitter.php');
-	echo $follow_count;
-	$data = json_decode($follow_count, true);
-	echo $data;
-	$followers_count = $data['followers_count'];
-	//echo $followers_count;
-	*/
-
-/*
-function getTwitterFollowers($screenName = 'wpbeginner') {
-    // some variables
-    $consumerKey = 'YOUR_CONSUMER_KEY';
-    $consumerSecret = 'YOUR_CONSUMER_SECRET';
-    $token = get_option('cfTwitterToken');
- 
-    // get follower count from cache
-    $numberOfFollowers = get_transient('cfTwitterFollowers');
- 
-    // cache version does not exist or expired
-    if (false === $numberOfFollowers) {
-        // getting new auth bearer only if we don't have one
-        if(!$token) {
-            // preparing credentials
-            $credentials = $consumerKey . ':' . $consumerSecret;
-            $toSend = base64_encode($credentials);
- 
-            // http post arguments
-            $args = array(
-                'method' => 'POST',
-                'httpversion' => '1.1',
-                'blocking' => true,
-                'headers' => array(
-                    'Authorization' => 'Basic ' . $toSend,
-                    'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8'
-                ),
-                'body' => array( 'grant_type' => 'client_credentials' )
-            );
- 
-            add_filter('https_ssl_verify', '__return_false');
-            $response = wp_remote_post('https://api.twitter.com/oauth2/token', $args);
- 
-            $keys = json_decode(wp_remote_retrieve_body($response));
- 
-            if($keys) {
-                // saving token to wp_options table
-                update_option('cfTwitterToken', $keys->access_token);
-                $token = $keys->access_token;
-            }
-        }
-        // we have bearer token wether we obtained it from API or from options
-        $args = array(
-            'httpversion' => '1.1',
-            'blocking' => true,
-            'headers' => array(
-                'Authorization' => "Bearer $token"
-            )
-        );
- 
-        add_filter('https_ssl_verify', '__return_false');
-        $api_url = "https://api.twitter.com/1.1/users/show.json?screen_name=$screenName";
-        $response = wp_remote_get($api_url, $args);
- 
-        if (!is_wp_error($response)) {
-            $followers = json_decode(wp_remote_retrieve_body($response));
-            $numberOfFollowers = $followers->followers_count;
-        } else {
-            // get old value and break
-            $numberOfFollowers = get_option('cfNumberOfFollowers');
-            // uncomment below to debug
-            //die($response->get_error_message());
-        }
- 
-        // cache for an hour
-        set_transient('cfTwitterFollowers', $numberOfFollowers, 1*60*60);
-        update_option('cfNumberOfFollowers', $numberOfFollowers);
-    }
- 
-    return $numberOfFollowers;
-}
-*/
 ?>
